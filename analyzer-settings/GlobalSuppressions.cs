1256646e7b9ea0eca5e119a6e1e90d5c3593d0d8
NoP77svk <nop77svk@gmail.com>
2021-08-25 10:19:47 +0200

    🔧 global analyzer suppressions sync'd with other solutions

--- analyzer-settings/GlobalSuppressions.cs
+++ analyzer-settings/GlobalSuppressions.cs
@@ -4,6 +4,7 @@
 [assembly: SuppressMessage("Style", "AD0001")] // Exception within StyleCop.Analyzers
 
 [assembly: SuppressMessage("Style", "IDE0057")] // Substring can be simplified
+[assembly: SuppressMessage("Style", "IDE0060")] // Remove unused parameter // 2do!
 [assembly: SuppressMessage("Style", "IDE0090")] // Use new (...)
 
 [assembly: SuppressMessage("Style", "CA2208")] // Wrong paramName to Argument...Exception
@@ -12,7 +13,10 @@
 [assembly: SuppressMessage("Style", "SA1011")] // Closing square bracket should be followed by a space
 [assembly: SuppressMessage("Style", "SA1101")] // Prefix local calls with this
 [assembly: SuppressMessage("Style", "SA1111")] // Closing parenthesis should be on line of last parameter
+// [assembly: SuppressMessage("Style", "SA1118")] // parameter spans multiple lines // 2do!
 [assembly: SuppressMessage("Style", "SA1122")] // Use string.Empty // 2do!
+[assembly: SuppressMessage("Style", "SA1201")] // Enum should not follow a class
+[assembly: SuppressMessage("Style", "SA1300")] // Element ... should begin with an uppercase letter
 [assembly: SuppressMessage("Style", "SA1309")] // Field should not begin with an underscore
 [assembly: SuppressMessage("Style", "SA1413")] // Use trailing comma in multi-line initializers
 [assembly: SuppressMessage("Style", "SA1503")] // Braces should not be ommitted
@@ -21,7 +25,9 @@
 
 [assembly: SuppressMessage("Style", "SA0001")] // XML comment analysis disabled
 [assembly: SuppressMessage("Style", "SA1600")] // Elements should be documented
+[assembly: SuppressMessage("Style", "SA1602")] // Enumeration items should be documented
 [assembly: SuppressMessage("Style", "SA1604")] // Element doc should have summary
 [assembly: SuppressMessage("Style", "SA1611")] // Missing parameter doc
 [assembly: SuppressMessage("Style", "SA1615")] // Missing return value doc
+[assembly: SuppressMessage("Style", "SA1629")] // Documentation text should end with a period
 [assembly: SuppressMessage("Style", "SA1633")] // Missing file header
