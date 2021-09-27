c89be52dd3908c721be3270d34b1c7b50949bbcf
NoP77svk <nop77svk@gmail.com>
2021-09-27 17:22:02 +0200

    * IEnumerable chunking enhanced with total chunk measure info on output

--- nop77svk-misc/Collections.Generic/IEnumerableChunkExt.cs
+++ nop77svk-misc/Collections.Generic/IEnumerableChunkExt.cs
@@ -6,7 +6,7 @@
 
     public static class IEnumerableChunkExt
     {
-        public static IEnumerable<List<TElement>> ChunkByMeasure<TElement>(
+        public static IEnumerable<ChunkOf<TElement>> ChunkByMeasure<TElement>(
             this IEnumerable<TElement> self,
             Func<TElement, int> calculateElementMeasure,
             int chunkThresholdMeasure
@@ -15,8 +15,11 @@
             if (chunkThresholdMeasure <= 0)
                 throw new ArgumentOutOfRangeException(nameof(chunkThresholdMeasure), chunkThresholdMeasure, "Chunk threshold measure must be positive integer");
 
-            List<TElement>? result = null;
-            int totalElementMeasure = 0;
+            ChunkOf<TElement> result = new ChunkOf<TElement>()
+            {
+                Chunk = null,
+                TotalChunkMeasure = 0
+            };
             int elementIndex = 0;
 
             foreach (TElement element in self)
@@ -27,31 +30,101 @@
                 if (elementMeasure < 0)
                     throw new ValueConstraintViolatedException($"Negative calculated element[{elementIndex}] measure");
 
-                if (totalElementMeasure + elementMeasure > chunkThresholdMeasure)
+                if (result.TotalChunkMeasure + elementMeasure > chunkThresholdMeasure)
                 {
-                    if (result == null)
+                    if (result.Chunk == null)
                         throw new ValueConstraintViolatedException($"Element[{elementIndex}] measure {elementMeasure} over chunking threshold {chunkThresholdMeasure}");
 
                     yield return result;
-                    result = null;
+
+                    result.Chunk = null;
+                    result.TotalChunkMeasure = 0;
                 }
 
-                if (result == null)
-                    result = new List<TElement>();
+                if (result.Chunk == null)
+                {
+                    result = new ChunkOf<TElement>()
+                    {
+                        Chunk = new List<TElement>(),
+                        TotalChunkMeasure = 0
+                    };
+                }
 
-                result.Add(element);
+                result.Chunk.Add(element);
+                result.TotalChunkMeasure += elementMeasure;
             }
 
-            if (result != null)
+            if (result.Chunk != null)
                 yield return result;
         }
 
-        public static IEnumerable<List<TElement>> ChunkByCount<TElement>(
+        public static IEnumerable<ChunkOf<TElement>> ChunkByCount<TElement>(
             this IEnumerable<TElement> self,
             int chunkThresholdElementCount
         )
         {
             return self.ChunkByMeasure(_ => 1, chunkThresholdElementCount);
+        }
+
+        public static async IAsyncEnumerable<ChunkOf<TElement>> ChunkByMeasure<TElement>(
+            this IAsyncEnumerable<TElement> self,
+            Func<TElement, int> calculateElementMeasure,
+            int chunkThresholdMeasure
+        )
+        {
+            if (chunkThresholdMeasure <= 0)
+                throw new ArgumentOutOfRangeException(nameof(chunkThresholdMeasure), chunkThresholdMeasure, "Chunk threshold measure must be positive integer");
+
+            ChunkOf<TElement> result = new ChunkOf<TElement>()
+            {
+                Chunk = null,
+                TotalChunkMeasure = 0
+            };
+            int elementIndex = 0;
+
+            await foreach (TElement element in self)
+            {
+                elementIndex++;
+
+                int elementMeasure = calculateElementMeasure(element);
+                if (elementMeasure < 0)
+                    throw new ValueConstraintViolatedException($"Negative calculated element[{elementIndex}] measure");
+
+                if (result.TotalChunkMeasure + elementMeasure > chunkThresholdMeasure)
+                {
+                    if (result.Chunk == null)
+                        throw new ValueConstraintViolatedException($"Element[{elementIndex}] measure {elementMeasure} over chunking threshold {chunkThresholdMeasure}");
+
+                    yield return result;
+
+                    result.Chunk = null;
+                    result.TotalChunkMeasure = 0;
+                }
+
+                if (result.Chunk == null)
+                {
+                    result = new ChunkOf<TElement>()
+                    {
+                        Chunk = new List<TElement>(),
+                        TotalChunkMeasure = 0
+                    };
+                }
+
+                result.Chunk.Add(element);
+                result.TotalChunkMeasure += elementMeasure;
+            }
+
+            if (result.Chunk != null)
+                yield return result;
+        }
+
+        public static async IAsyncEnumerable<ChunkOf<TElement>> ChunkByCount<TElement>(
+            this IAsyncEnumerable<TElement> self,
+            int chunkThresholdElementCount
+        )
+        {
+            await foreach (ChunkOf<TElement> element in self.ChunkByMeasure(_ => 1, chunkThresholdElementCount))
+                yield return element;
         }
     }
 }
