016a65e35ffe89c644181198e11fbefcfdf92cdc
Peter Hraško <peter_hrasko@ri-rpc.sk>
2023-12-20 12:07:18 +0100

    🧹 enumerable chunk made a subclass of the main extension class

--- Linq/ChunkOf.cs
+++ Linq/ChunkOf.cs
@@ -2,9 +2,12 @@
 {
     using System.Collections.Generic;
 
-    public struct ChunkOf<TElement>
+    public static partial class EnumerableChunkingExt
     {
-        public List<TElement>? Chunk;
-        public int TotalChunkMeasure;
+        public struct ChunkOf<TElement>
+        {
+            public List<TElement>? Chunk;
+            public int TotalChunkMeasure;
+        }
     }
 }
